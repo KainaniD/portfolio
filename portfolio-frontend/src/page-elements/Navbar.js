f9c22c26ae32871c566ac49d42f92d80bc6580dd
KainaniD <kainanime2@gmail.com>
2024-06-18 23:59:33 -0700

    added bold navigation header

--- portfolio-frontend/src/page-elements/Navbar.js
+++ portfolio-frontend/src/page-elements/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
       <div className="navbar hidden md:flex flex-col gap-4 fixed top-0 right-0 p-4 text-right">
-            <h2>Navigation</h2>
+            <h2 className="font-bold">Navigation</h2>
             <a href="#pageLanding" className="text-2xl hover:text-4xl">Home</a>
             <a href="#pageAbout" className="text-2xl hover:text-4xl">About</a>
             <a href="#pageProjects" className="text-2xl hover:text-4xl">Projects</a>
